4e4cc1f7a86987de54b464ebf33c71f31ac8784f
Wassel ALAZHAR <walazhar@octo.com>
2018-03-27 18:14:04 +0200

    Add guidelines comments

--- good_friends_tests/test_good_friends.py
+++ good_friends_tests/test_good_friends.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
+# Think -> Red -> Green -> Refactor
+#
+# Guideline 1: Always start with outputs
+# 
+# Guideline 2: Behavior slicing
+#
+# Guideline 3: Simplify
+#
 class TestGoodFriends(TestCase):
 
     def setUp(self):
