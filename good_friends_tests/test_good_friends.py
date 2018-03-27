ecb8753fb16c6de1b2e7cc6db9592c777c0dd84e
Wassel ALAZHAR <walazhar@octo.com>
2018-03-27 17:51:31 +0200

    init a python project with failing unit test

---
+++ good_friends_tests/test_good_friends.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+class TestGoodFriends(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_we_can_run_tests(self):
+        # Assert
+        self.assertEquals(1,1)
